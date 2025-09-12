f9d2c69ec9ae7d9aadac9085722bef654416af51
Shanghai <phuc2348hong@gmail.com>
2025-09-12 07:33:40 +0700

    restore old App.js for doing new ex

--- my-first-app/src/App.js
+++ my-first-app/src/App.js
@@ -1,16 +1,25 @@
+import logo from './logo.svg';
 import './App.css';
-import ReactDOM from 'react-dom/client';
 
 function App() {
-    return (
-      <div className="App-container">
-        <h1>Chào Mừng Đến Với Thế Giới Của Anh</h1>
-      </div>
-    );
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
-export default App;
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-    <App />
-);
+export default App;
