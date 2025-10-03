546fa513227d3ff9d39bea07c41c6a249a400e48
Shanghai <phuc2348hong@gmail.com>
2025-10-03 09:01:03 +0700

    small thing

--- LAB2/ex1/src/component/NavBar.jsx
+++ LAB2/ex1/src/component/NavBar.jsx
@@ -66,54 +66,6 @@ function Navbar() {
             </button>
           </form>
         </div>
-        {/* <button
-          type="button"
-          className="btn btn-primary"
-          data-bs-toggle="modal"
-          data-bs-target="#exampleModal"
-        >
-          Launch demo modal
-        </button> */}
-
-        {/* modal */}
-        {/* <div
-          className="modal fade"
-          id="exampleModal"
-          tabIndex="-1"
-          role="dialog"
-          aria-labelledby="exampleModalLabel"
-          aria-hidden="true"
-        >
-          <div className="modal-dialog" role="document">
-            <div className="modal-content">
-              <div className="modal-header">
-                <h5 className="modal-title" id="exampleModalLabel">
-                  Modal title
-                </h5>
-                <button
-                  type="button"
-                  className="btn-close"
-                  data-bs-dismiss="modal"
-                  aria-label="Close"
-                >
-                </button>
-              </div>
-              <div className="modal-body">...</div>
-              <div className="modal-footer">
-                <button
-                  type="button"
-                  className="btn btn-secondary"
-                  data-bs-dismiss="modal"
-                >
-                  Close
-                </button>
-                <button type="button" className="btn btn-primary">
-                  Save changes
-                </button>
-              </div>
-            </div>
-          </div>
-        </div> */}
       </div>
     </nav>
   );
